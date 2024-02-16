5d48de3d08607b2df766868e8b0467a6e14405cc
Toby Dragon <6741052+tobydragon@users.noreply.github.com>
2024-02-16 10:21:10 -0500

    Initial commit

---
+++ tests/poem_tests.py
@@ -0,0 +1,12 @@
+import json
+import pytest
+from poem import PoemWhole, PoemByLine
+
+
+def test_poem_whole():
+    raise RuntimeError("Not implemented yet")
+
+
+def test_poem_by_line():
+    raise RuntimeError("Not implemented yet")
+
